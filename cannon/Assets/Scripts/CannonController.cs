8a2ca0d709023f58d2b1138156767d84801637e1
davekaul <davekaul@gmail.com>
2021-09-19 12:20:23 -0400

    Add base movement to cannon

--- cannon/Assets/Scripts/CannonController.cs
+++ cannon/Assets/Scripts/CannonController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float PositionMagnitude;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Vector3 _targetPosition = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _targetPosition += transform.forward * PositionMagnitude * Time.deltaTime;
+        }
         
+        if (Input.GetKey(KeyCode.S))
+        {
+            _targetPosition -= transform.forward * PositionMagnitude * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _targetPosition -= transform.right * PositionMagnitude * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _targetPosition += transform.right * PositionMagnitude * Time.deltaTime;
+        }
+
+        if (_targetPosition.magnitude > 0f)
+        {
+            transform.Translate(_targetPosition);
+        }
     }
 }

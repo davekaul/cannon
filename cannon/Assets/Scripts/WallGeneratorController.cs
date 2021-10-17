81939ac76f68a28c6ba9e621d3dbf7c5714b9ab8
davekaul <davekaul@gmail.com>
2021-10-17 12:38:54 -0400

    Add first pass of wall width variation

--- cannon/Assets/Scripts/WallGeneratorController.cs
+++ cannon/Assets/Scripts/WallGeneratorController.cs
@@ -8,10 +8,11 @@ public class WallGeneratorController : MonoBehaviour
     public GameObject Brick;
     [Range(1, 100)] public int WallRow;
     [Range(1, 100)] public int WallCol;
-    [Range(1, 10)] public int WallPadding;
+    [Range(0.1f, 0.5f)] public float WallPadding;
+    [Range(0.7f, 0.99f)] public float WallCapTolerance;
 
     private float _brickWidth = 1.05f;
-    private float _brickHeight = 0.501f;
+    private float _brickHeight = 0.501f;    
    
     private void Start()
     {
@@ -22,16 +23,29 @@ public class WallGeneratorController : MonoBehaviour
     {      
         for(var i = 0; i < WallRow; i++)
         {
+            var currentRowPosition = 0f;
+
             for (var j = ((-WallCol / 2f)) + (_brickWidth / 2f); j < (WallCol / 2f); j++)
-            {
-                var x = WallPadding * _brickWidth * j;
-                var y = _brickHeight * i;
-                
+            {               
+                // Creation
                 var brick = Instantiate(Brick);
                 BrickList.Add(brick);
-
                 brick.transform.SetParent(transform);
-                brick.transform.position = transform.position + new Vector3(x, y, 0f);               
+                
+                // Calc trans
+                var brickWidth = Random.Range(_brickWidth * 0.7f, _brickWidth * 1.3f);                
+                var x = WallPadding + brickWidth;
+                var y = _brickHeight * i;
+                
+                brick.transform.position = new Vector3(currentRowPosition, transform.position.y + y, transform.position.z);
+                brick.transform.localScale = new Vector3(x, brick.transform.localScale.y, brick.transform.localScale.z);
+
+                currentRowPosition += x;
+
+                if (currentRowPosition >= (WallCol / 2f) * WallCapTolerance)
+                {
+                    break;
+                }
             }
         }
 
